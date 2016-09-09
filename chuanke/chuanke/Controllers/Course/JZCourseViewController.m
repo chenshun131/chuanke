5673f50cc057e3bab384ebcc10959d34b3847bc1
chenshun131 <1539831174@qq.com>
2016-09-09 10:04:51 +0800

    提交第一个页面数据

--- chuanke/chuanke/Controllers/Course/JZCourseViewController.m
+++ chuanke/chuanke/Controllers/Course/JZCourseViewController.m
@@ -9,6 +9,18 @@
 #import "JZCourseViewController.h"
 
 @interface JZCourseViewController ()
+{
+    NSMutableArray *_focusListArray;/**< 第一个轮播数据 */
+    NSMutableArray *_focusImgurlArray;/**< 第一个轮播图片URL数据 */
+    NSMutableArray *_courseListArray;/**< 列表数据 */
+    NSMutableArray *_albumListArray;/**< 第二个轮播数据 */
+    NSMutableArray *_albumImgurlArray;/**< 第二个轮播图片URL数据 */
+    
+    NSInteger _type;/**< segment */
+    
+    NSMutableArray *_classCategoryArray;/**< 课程分类数组 */
+    NSMutableArray *_iCategoryListArray;/**< 课程类型 */
+}
 
 @end
 
@@ -18,12 +30,44 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self initData];
+    [self setNav];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)initData
+{
+    _focusListArray = [[NSMutableArray alloc] init];
+    _focusImgurlArray = [[NSMutableArray alloc] init];
+    _courseListArray = [[NSMutableArray alloc] init];
+    _albumListArray = [[NSMutableArray alloc] init];
+    _albumImgurlArray = [[NSMutableArray alloc] init];
+    
+    _type = 0;
+    
+    // 读取plist文件
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"classCategory" ofType:@"plist"];
+    _classCategoryArray = [[NSMutableArray alloc] initWithContentsOfFile:plistPath];
+    // 课程类型
+    NSString *iCategoryListPath = [[NSBundle mainBundle] pathForResource:@"iCategoryList" ofType:@"plist"];
+    _iCategoryListArray = [[NSMutableArray alloc] initWithContentsOfFile:iCategoryListPath];
+}
+
+- (void)setNav
+{
+    UIView *backView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screen_width, 98)];
+    backView.backgroundColor = navigationBarColor;
+    [self.view addSubview:backView];
+    
 }
 
 /*
