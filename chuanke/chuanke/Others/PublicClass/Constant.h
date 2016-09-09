4f5013f7f304ea1ad9c593e622f4dfac848acd2b
chenshun131 <1539831174@qq.com>
2016-09-09 08:37:50 +0800

    添加 pch 文件

---
+++ chuanke/chuanke/Others/PublicClass/Constant.h
@@ -0,0 +1,18 @@
+//
+//  Constant.h
+//  chuanke
+//
+//  Created by dfjty on 16/9/9.
+//  Copyright © 2016年 chenshun131. All rights reserved.
+//
+
+#ifndef Constant_h
+#define Constant_h
+
+#define UID @"4597633"
+#define VERSION @"2.4.1.2"
+
+// 友盟Appkey
+#define UMAPPKEY @"57d17a61e0f55aa55f0020ce"
+
+#endif /* Constant_h */
